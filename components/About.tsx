c6026a03db33e1f3672de7fa2c3a4577fbc2bb2f
stuey-b <stuartwbartlett@gmail.com>
2023-01-02 21:32:04 +0900

    upgrade img tags to <Image/>

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 type Props = {}
 
@@ -6,8 +7,13 @@ const About = (props: Props) => {
   return (
     <section className='px-14 flex flex-col justify-center bg-emerald-100 h-screen'>
       <h2 className='text-4xl font-black text-slate-900'>About Me<span className='text-pink-500'>.</span></h2>
-      <div className='  gap-4'>
-      <img className="mx-auto -mb-8" src="logo.png" alt="logo" />
+      <div className='gap-4'>
+        <Image className='-mb-12'
+        src="/logo.png"
+        alt='logo'
+        width={512}
+        height={512}
+        />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos eaque ab in officiis dicta enim. Ab necessitatibus iusto ad accusamus deserunt eos nemo provident dolorem, exercitationem explicabo blanditiis enim voluptate maiores porro quaerat tenetur accusantium ratione laboriosam repellat atque mollitia eveniet doloribus? Itaque nam laboriosam fugit aliquam facere amet nihil nobis! Amet aliquam eaque consequuntur quod quibusdam vitae doloremque nemo animi, aliquid sequi eligendi deleniti omnis libero! Velit sit ratione fuga doloremque aspernatur laboriosam earum debitis nostrum officiis? Mollitia laboriosam ipsam cum fuga qui sed ex eos natus voluptatum cumque, repellat eum libero tempore excepturi nemo ipsum assumenda iure at nobis dolorum. Ad alias reprehenderit nam quo eius expedita fugiat totam quam, neque accusantium magni vel exercitationem sapiente eligendi, inventore repudiandae cumque vero ipsam temporibus! Unde autem consequatur repudiandae voluptatibus in velit mollitia ad, architecto ducimus odit assumenda veniam recusandae enim cumque officiis repellendus, dignissimos quod maxime dicta libero neque id! Vero nam, cum voluptatum necessitatibus perspiciatis vel provident doloremque error aliquam maxime quia iure sint in asperiores impedit! Quae possimus quo optio expedita reiciendis iste earum sapiente?</p>
 
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { motion } from "framer-motion";
 
 type Props = {};
@@ -9,9 +10,13 @@ const Header = (props: Props) => {
       className="flex absolute items-center justify-between h-32 pt-4 pr-14 w-screen bg-transparent"
     >
       <a className="" href="#">
-        <img className="h-32 pl-4" src="logo.png" alt="logo" />
+      <Image
+        src="/logo.png"
+        alt='test'
+        width={128}
+        height={512}
+        />
         </a>
-  
       <button className="space-y-2" id="hamburger-menu">
         <div className="w-10 h-1 rounded bg-indigo-800"></div>
         <div className=" ml-2 w-8 h-1 rounded bg-indigo-800"></div>
